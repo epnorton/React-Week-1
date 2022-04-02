0918d39841b9b4283c17165edf280b3e5e981659
Elizabeth Norton <96452970+epnorton@users.noreply.github.com>
2022-04-02 09:52:32 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Login from './components/login';
+import Navigation from './components/navigation';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const login = React.createElement(Login, {}, null);
+const navigation = React.createElement(Navigation, {}, null);
+ReactDOM.render(
+    [login, navigation],
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
